057a76170f178450931d16e368d9f8cde1be321c
MakerSquare Student <students@themakersquare.com>
2013-09-30 14:42:03 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LionWitch::Application.config.secret_key_base = '9e136543b7c17a8f544f74547a8bdcf04b819a763e0b756b87083337e4ec59c11437bdf2d491a0a011107e5659404de81d72187ab1178e3bafffb313e684b6d6'
